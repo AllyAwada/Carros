9ef091a6bb88ac0e8d028c65386ff53f4c8f623f
AllyAwada <Allyhawada13@gmail.com>
2024-03-24 21:39:48 -0300

    Projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import Luxury from "./components/Luxuosos"
 import Luxuosos from "./components/Luxuosos"
 import Sedans from "./components/Sedans"
 import Suvs from "./components/Suvs"
 export default function App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center itens-center">
-      <section className="lg:flex max-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
         <Sedans />
         <Suvs />
         <Luxuosos />
-      </section>
+      </div>
     </main>
   )
 }
