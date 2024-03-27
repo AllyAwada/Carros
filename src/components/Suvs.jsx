d9cbbe1fc57380f40c77e077bcc664e524379480
AllyAwada <allyhawada@icloud.com>
2024-03-27 14:41:37 -0300

    Projeto finalizado

--- src/components/Suvs.jsx
+++ src/components/Suvs.jsx
@@ -2,12 +2,12 @@ import iconSuvs from "../assets/icon-suvs.svg"
 
 function Suvs() {
     return (
-        <div className="bg-green p-12">
+        <section className="bg-green p-12">
             <img src={iconSuvs} alt="Icone dos carros SUV." className=" mb-7" />
             <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Suvs</h2>
             <p className="font-lexend mb-7 text-offwhite lg:mb-20">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
             <a href="https://www.webmotors.com.br/suvs" target="_blank" className="font-lexend bg-offwhitebg block w-fit px-10 py-3 rounded-3xl text-green border-2 border-offwhitebg hover:bg-green hover:text-offwhitebg">Ver mais</a>
-        </div>
+        </section>
     )
 }
 
